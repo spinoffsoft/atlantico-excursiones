471d0db020a37da4cb6e4f6cdff37706b4930292
Atlantico Excursiones <70583964+tozi007@users.noreply.github.com>
2026-07-18 19:41:38 +0100

    Initialize main.py with business metrics and plots

---
+++ src/main.py
@@ -0,0 +1,14 @@
+"""
+from business_metrics import BusinessMetrics
+
+business_metrics = BusinessMetrics()
+business_metrics.run()
+
+"""
+
+from plots_pro import ProfessionalPlots
+
+plots = ProfessionalPlots()
+plots.run()
+
+
